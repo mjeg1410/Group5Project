ea1e2535fb281f60644b51788950f32170b46169
abukharis <sa.abukharis@gmail.com>
2020-04-08 22:15:04 +0000

    random_charecter

---
+++ service3/application/routes.py
@@ -0,0 +1,9 @@
+from application import app
+from flask import request, response
+from random import randint, random
+from string import string 
+@app.route('/charecter', methods=['GET', 'POST'])
+def random_charecter():
+    lucky_charecter= ''.join([random.choice(string.ascii_letters ) for n in range(3)])
+    return response(lucky_charecter)
+
