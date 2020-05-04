46e3a52ac4af706370aa42562bf2bbfdc95d30e9
mjeg1410 <33596231+mjeg1410@users.noreply.github.com>
2020-05-04 13:32:45 +0100

    Update models.py

--- service1/application/models.py
+++ service1/application/models.py
@@ -1,4 +1,4 @@
 from application import db
 class Fortunes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fortune = db.Column(db.String,nullalbe=False)
+    fortune = db.Column(db.String,nullable=False)
