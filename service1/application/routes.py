cff6cd6d9422186a3de8acb43c0d68756783edec
abukharis <sa.abukharis@gmail.com>
2020-05-06 21:27:48 +0000

    routes_fixes

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -16,14 +16,14 @@ def fortune():
     token =tokenadd.text
     fortunes= ["You will have a terrible day","You will have a mediocre day","You will have a great day"]
     if token[0] == "1":
-        fortuneshown =Fortunes(fortune=fortunes[0]) 
+        #fortuneshown =Fortunes(fortune=fortunes[0]) 
         fortuneshown=fortunes[0]
     elif token[0] == "9":
-        fortuneshown = Fortunes(fortune=fortunes[2])
+        #fortuneshown = Fortunes(fortune=fortunes[2])
         fortuneshown=fortunes[2]
     else:
         fortuneshown=fortunes[1]
-        fortuneshown = Fortunes(fortune=fortunes[1])
+        #fortuneshown = Fortunes(fortune=fortunes[1])
     #return Response(str(fortuneshown), mimetype="text/plain")
        # db.session.add(fortuneshown)
         #db.session.commit()
