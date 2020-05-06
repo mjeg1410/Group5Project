ef617aa70dd74fce8518b34b96c344e12af10cf6
abukharis <sa.abukharis@gmail.com>
2020-05-06 18:34:22 +0000

    working but notwriting the out to db

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,7 +1,7 @@
 from application import app
 from application import db
 from application.models import Fortunes
-from flask import request, render_template, redirect,url_for
+from flask import request, render_template, redirect,url_for, Response
 import requests
 from random import randint, random
 import string
@@ -17,18 +17,18 @@ def fortune():
     fortunes= ["You will have a terrible day","You will have a mediocre day","You will have a great day"]
     if token[0] == "1":
         fortuneshown =Fortunes(fortune=fortunes[0]) 
-        #fortuneshown=fortunes[0]
+        fortuneshown=fortunes[0]
     elif token[0] == "9":
         fortuneshown = Fortunes(fortune=fortunes[2])
-        #fortuneshown=fortunes[2]
+        fortuneshown=fortunes[2]
     else:
-        #fortuneshown=fortunes[1]
+        fortuneshown=fortunes[1]
         fortuneshown = Fortunes(fortune=fortunes[1])
-    #return Response(fortuneshown,mimetype="text/plain")
-        db.session.add(fortuneshown)
-        db.session.commit()
-    return render_template('fortune.html', title='Fortune', fortuneshown=fortuneshown)
-#def output(fortuneshown):
-   # db.session.add(fortuneshown)
-    #db.session.commit()
+    return Response(str(fortuneshown), mimetype="text/plain")
+        #db.session.add(fortuneshown)
+        #db.session.commit()
     #return render_template('fortune.html', title='Fortune', fortuneshown=fortuneshown)
+def output(fortuneshown):
+    db.session.add(fortuneshown)
+    db.session.commit()
+    return render_template('fortune.html', title='Fortune', fortuneshown=fortuneshown)
